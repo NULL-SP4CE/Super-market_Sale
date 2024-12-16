513189e0c07cde838a687fd13fa70de70f57c5f2
NULL-SP4CE <aungmyatthu28196@gmail.com>
2024-12-16 23:00:18 +0630

    Add files via upload

--- Super_market_sale.sql
+++ Super_market_sale.sql
@@ -1,18 +1,3 @@
-# Super-market Sales Data Analysis using PostgreSQL
-
-## Overview
-This project involves a comprehensive analysis of Super-market sale data using PostgreSQL. The goal is to extract valuable insights and answer various business questions based on the dataset. The following README provides a detailed account of the project's objectives, problems and solutions.
-
-
-## Dataset
-
-The data for this project is sourced from the Kaggle dataset:
-
-- **Dataset Link:** [Super-market Sale](https://www.kaggle.com/datasets/arunjangir245/super-market-sales)
-
-## CREATING_TABLE
-
-```sql
 DROP TABLE IF EXISTS super_market;
 CREATE TABLE super_market (
     Invoice_ID VARCHAR(15) PRIMARY KEY,
@@ -32,27 +17,16 @@ CREATE TABLE super_market (
     gross_income DECIMAL,
     Rating DECIMAL
 );
-```
---Check column data type
-SELECT * FROM super_market;
 
 
-## Problems and Solutions 
-
-### (1) Finding average customer rating of product_line by branch.
-
-```sql
+--(1) Finding average customer rating of product_line by branch.
 SELECT CONCAT(city,' branch'), product_line, 
     ROUND(AVG(rating),3) AS average_rating
 FROM super_market
     GROUP BY 1,2
     ORDER BY 1, 3 DESC ;
-```
 
-
-### (2) Finding total sale count of all prouct_line by time periods(Morning, Afternoon and evening).
-
-```sql
+--(2) Finding total sale count of all prouct_line by time periods(Morning, Afternoon and evening).
 WITH CTE_Time_periods_order AS (
 SELECT 
     product_line,
@@ -74,24 +48,16 @@ FROM CTE_time_periods_order
             WHEN time_periods = 'evening' THEN 3
             ELSE 4 
          END NULLS LAST ;
-```
 
-
-### (3) finding total sale item count by payment type in 2019
-
-```sql
+--(3) finding total sale item count by payment type in 2019
 SELECT 
     EXTRACT (year FROM sale_date) AS year,
     payment AS payment_type,
     SUM(quantity) AS total_item
 FROM super_market
     GROUP BY 1, 2 ;
-```
 
-
-### (4) Finding total sale amount and items on each product line by gender
-
-```sql
+-- (4) Finding total sale amount and items on each product line by gender
 SELECT product_line,
     gender,
     COUNT(quantity) AS total_item,
@@ -100,15 +66,12 @@ FROM super_market
     GROUP BY 1, 2
     ORDER BY 1, 3 DESC ;
 
-```
 
-
-### (5) Finding total gross income on each product by date.
-    	And giving bonus to staff on gross income --> Electronic accessories = 5%, Fashion accessories= 8%,
-   	Food and beverages =5%, Health and beauty =6%, Home and lifestyle=10%, Sports and travel=20%,
-    	also finding final_gross_income(total_gross_income - giving_bonus_to_staff) 
-
-```WITH CTE_Final_gross AS (
+-- (5) Finding total gross income on each product by date.
+--     And giving bonus to staff on gross income --> Electronic accessories = 5%, Fashion accessories= 8%,
+--     Food and beverages =5%, Health and beauty =6%, Home and lifestyle=10%, Sports and travel=20%,
+--     also finding final_gross_income(total_gross_income - giving_bonus_to_staff) 
+WITH CTE_Final_gross AS (
     SELECT 
         date,
         product_line,
@@ -133,37 +96,29 @@ FROM super_market
 SELECT *,
     total_gross_income - giving_bonus_to_staff AS Final_Gross_income
 FROM CTE_final_gross ;
-```
 
 
-### (6) Finding total customer and total spent amount on customer type by gender
-
-```sql
+--(6) Finding total customer and total spent amount on customer type by gender 
 SELECT gender, customer_type,
     COUNT(customer_type) AS total_customer,
     SUM(total) AS total_amount 
 FROM super_market 
     GROUP BY 1, 2
-    ORDER BY  1 ; 
-```
+    ORDER BY  1 ;
 
-
-### (7) Finding average spend for food on gender by city
-
-```sql
+    
+-- (7) Finding average spend for food on gender by city
 SELECT city, gender, ROUND(AVG(total), 2) AS average_spend_for_food
 FROM super_market
     GROUP BY 1, 2
     ORDER BY 1, 3 DESC ;
-```
 
-### (8) if supermarket gift to customers 
-	total quanties greater than 5 gift unbrella in mandalay
-	total quanties greater than 5 gift tote bag in Yangon
-	total quantites graeter than 3 gift discount cupon in naypyidaw
-	finding gross income by deducing promption amount(unbreall=3, tote bag=1.5, cupon=0.5).
 
-```sql
+-- (8) if supermarket gift to customers 
+-- total quanties greater than 5 gift unbrella in mandalay
+-- total quanties greater than 5 gift tote bag in Yangon
+-- total quantites graeter than 3 gift discount cupon in naypyidaw
+-- finding gross income by deducing promption amount(unbreall=3, tote bag=1.5, cupon=0.5).
 WITH CTE_gift AS (
     SELECT city, quantity,
         CASE 
@@ -182,12 +137,8 @@ SELECT *,
     END AS Final_gross_income
 FROM CTE_gift ;
 
-```
 
-
-### (9) Giving customers 10% off who make purchase with Ewallet in Afternoon
-
-```sql
+-- (9) Giving customers 10% off who make purchase with Ewallet in Afternoon
 SELECT *, ROUND(total * 0.9, 2) AS Ten_Percent_discounted_amount
 FROM 
     (SELECT payment,
@@ -200,19 +151,10 @@ FROM
     FROM super_market) AS time_query
 WHERE time_periods = 'Afternoon' 
     AND payment = 'Ewallet' ;
-```
 
 
-### (10) Ranking gross income by highest to lowest 
-
-```sql
+-- (10) Ranking gross income by highest to lowest 
 SELECT city, product_line, quantity, gross_income, 
     DENSE_RANK () OVER(ORDER BY gross_income DESC) AS gross_income_ranking
 FROM super_market ;
-```
-
-
-## Conclusion
-
-This analysis provides a comprehensive view of Super-market's sale content and can help inform content strategy and decision-making.
 
